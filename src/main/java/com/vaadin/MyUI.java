4e503d07756bf9aafa2f1d61a91640fd7ceb6f9a
Teppo Nygren <teppo.nygren@iki.fi>
2017-03-14 20:25:34 +0200

    Layout testi

--- src/main/java/com/vaadin/MyUI.java
+++ src/main/java/com/vaadin/MyUI.java
@@ -4,19 +4,17 @@ import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.ExternalResource;
+import com.vaadin.server.ThemeResource;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.*;
 
 /**
- * This UI is the application entry point. A UI may either represent a browser window 
+ * This UI is the application entry point. A UI may either represent a browser window
  * (or tab) or some part of a html page where a Vaadin application is embedded.
  * <p>
- * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be
  * overridden to add component to the user interface and initialize non-component functionality.
  */
 @Theme("mytheme")
@@ -25,18 +23,56 @@ public class MyUI extends UI {
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         final VerticalLayout layout = new VerticalLayout();
-        
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
 
-        Button button = new Button("Click Me");
-        button.addClickListener( e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
-        });
-        
-        layout.addComponents(name, button);
-        
+        final Label Otsikko = new Label();
+        Otsikko.setCaption("Elokuvaraus");
+        layout.addComponent(Otsikko);
+        layout.setComponentAlignment(Otsikko, Alignment.TOP_CENTER);
+
+        final HorizontalLayout middleFrontPage = new HorizontalLayout();
+        layout.addComponent(middleFrontPage);
+
+        final VerticalLayout middleFrontPageleft = new VerticalLayout();
+        final VerticalLayout middleFrontPageright = new VerticalLayout();
+        middleFrontPage.addComponentsAndExpand(middleFrontPageleft, middleFrontPageright);
+
+        final HorizontalLayout leffaKortti1 = new HorizontalLayout();
+        final HorizontalLayout leffaKortti2 = new HorizontalLayout();
+        final HorizontalLayout leffaKortti3 = new HorizontalLayout();
+
+        Image kuva1 = new Image();
+        kuva1.setSource(new ThemeResource("../../elokuvajulisteet/moonlight.jpg"));
+        final VerticalLayout leffa1tiedotright = new VerticalLayout();
+        final VerticalLayout leffa1tiedotleft= new VerticalLayout();
+        final Label leffa1otsikko = new Label("Moonlight");
+        Button leffa1nappi = new Button("Varaa lippu");
+        leffa1tiedotright.addComponents(leffa1otsikko, leffa1nappi);
+        leffa1tiedotleft.addComponent(kuva1);
+        leffaKortti1.addComponents(leffa1tiedotleft, leffa1tiedotright);
+        middleFrontPageleft.addComponent(leffaKortti1);
+
+        Image kuva2 = new Image();
+        kuva2.setSource(new ThemeResource("../../elokuvajulisteet/vuosisadannaiset.jpg"));
+        final VerticalLayout leffa2tiedotright = new VerticalLayout();
+        final VerticalLayout leffa2tiedotleft= new VerticalLayout();
+        final Label leffa2otsikko = new Label("Vuosisadan Naiset");
+        Button leffa2nappi = new Button("Varaa lippu");
+        leffa2tiedotright.addComponents(leffa2otsikko, leffa2nappi);
+        leffa2tiedotleft.addComponent(kuva2);
+        leffaKortti2.addComponents(leffa2tiedotleft, leffa2tiedotright);
+        middleFrontPageleft.addComponent(leffaKortti2);
+
+        Image kuva3 = new Image();
+        kuva3.setSource(new ThemeResource("../../elokuvajulisteet/legobatman.jpg"));
+        final VerticalLayout leffa3tiedotright = new VerticalLayout();
+        final VerticalLayout leffa3tiedotleft= new VerticalLayout();
+        final Label leffa3otsikko = new Label("Lego Batman");
+        Button leffa3nappi = new Button("Varaa lippu");
+        leffa3tiedotright.addComponents(leffa3otsikko, leffa3nappi);
+        leffa3tiedotleft.addComponent(kuva3);
+        leffaKortti3.addComponents(leffa3tiedotleft, leffa3tiedotright);
+        middleFrontPageright.addComponent(leffaKortti3);
+
         setContent(layout);
     }
 
