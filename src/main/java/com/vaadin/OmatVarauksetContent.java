ae6da54cebea839f85a47451bb141fa810d13780
Mikko Metsäranta <misame@utu.fi>
2017-05-14 11:37:26 +0300

    Lisätty kommentteja

--- src/main/java/com/vaadin/OmatVarauksetContent.java
+++ src/main/java/com/vaadin/OmatVarauksetContent.java
@@ -2,7 +2,6 @@ package com.vaadin;
 
 import com.vaadin.data.Binder;
 import com.vaadin.server.ThemeResource;
-import com.vaadin.server.VaadinSession;
 import com.vaadin.spring.annotation.SpringComponent;
 import com.vaadin.ui.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @SpringComponent
 public class OmatVarauksetContent extends HorizontalLayout {
@@ -26,7 +24,7 @@ public class OmatVarauksetContent extends HorizontalLayout {
 
     List<Varaus> varaukset;
     Binder<Varaus> binder = new Binder<>(Varaus.class);
-    int kayttajanId;
+    private long id;
 
     private Grid<Varaus> varausGrid;
 
@@ -41,29 +39,52 @@ public class OmatVarauksetContent extends HorizontalLayout {
 
     private void setVaraukset(List<Varaus> varaukset) {
         removeAllComponents();
-        long id;
+
+        this.id = kirjautuneenKayttajanID();
+
+        if (this.id != 0) {
+            this.varaukset = new ArrayList<>();
+            // haetaan tietokannassa olleet varaukset ja lisätään ne this.varaukset
+            // listaan, jos varaus kuuluu kirjautuneelle käyttäjälle
+            for (int i = 0; i < varaukset.size(); i++) {
+                if (varaukset.get(i).getVaraajanId() == id) {
+                    this.varaukset.add(varaukset.get(i));
+                }
+            }
+            luoNakyma();
+        }
+    }
+
+    /**
+     *  Palauttaa kirjautuneen käyttäjän ID:n
+     *  @return käyttäjän ID tai 0 jos ei kirjautunutta käyttäjää
+     */
+    private long kirjautuneenKayttajanID() {
         try {
             KirjautumisKontrolli kirjautumisKontrolli = (KirjautumisKontrolli) getSession().getAttribute("kirjautumisKontrolli");
-            id = kirjautumisKontrolli.getKayttajaId();
+            return kirjautumisKontrolli.getKayttajaId();
         } catch (Exception e) {
-            id = 0;
+            return 0;
         }
-        this.varaukset = new ArrayList<>();
-        for (int i = 0; i < varaukset.size(); i++) {
-            if (varaukset.get(i).getVaraajanId() == id) {
-                this.varaukset.add(varaukset.get(i));
-            }
-        }
+    }
+
+    /**
+     *  Luo käyttäjän varauksista listan, jossa näkyy varattujen näytösten tiedot sekä varatut paikat
+     */
+    private void luoNakyma() {
         if (this.varaukset.size() == 0) {
             addComponent(new Label("Sinulla ei ole vielä varattuja elokuvalippuja!"));
         } else {
+            // naytoksienIdt listalla tarkistetaan, onko kyseisestä näytöksestä tehty jo "leffakortti" näkymään
             ArrayList<Integer> naytoksienIdt = new ArrayList<>();
             ArrayList<VerticalLayout> varauksienTiedot = new ArrayList<>();
             for (Varaus v : this.varaukset) {
+                // jos näytöksestä ei ole vielä tehty "leffakorttia", luodaan se tässä
                 if (!naytoksienIdt.contains(v.getNaytoksenId())) {
                     naytoksienIdt.add(v.getNaytoksenId());
 
                     Naytos n = new Naytos();
+                    // etsitään oikea näytös tietokannasta
                     for (Naytos nn : naytosRepository.findAll()) {
                         if (nn.getId() == v.getNaytoksenId()) {
                             n = nn;
@@ -71,11 +92,14 @@ public class OmatVarauksetContent extends HorizontalLayout {
                     }
 
                     Elokuva elokuva = new Elokuva();
+                    // etsitään näytökseen liittyvä elokuva tietokannasta
                     for (Elokuva e : this.elokuvaRepository.findAll()) {
                         if (v.getElokuvanNimi().equals(e.getNimi())) {
                             elokuva = e;
                         }
                     }
+
+                    // luodaan näkymä varauksen näytöksen tiedoista
                     final VerticalLayout leffaKortti = new VerticalLayout();
                     String nimi = elokuva.getNimi();
                     String kuvanOsoite = elokuva.getKuvanOsoite();
@@ -91,21 +115,16 @@ public class OmatVarauksetContent extends HorizontalLayout {
                     varauksienTiedot.add(leffaKortti);
 
                 } else {
+                    // Kun naytoksestä on jo olemassa "leffakortti,
+                    // etsitään naytoksienIdt listan avulla oikea komponentti, ja lisätään siihen paikkatieto
                     int indeksi = naytoksienIdt.indexOf(v.getNaytoksenId());
                     varauksienTiedot.get(indeksi).addComponent(new Label("Rivi: " + v.getRivi() + " Paikka: " + v.getPaikka()));
                 }
             }
-            for (VerticalLayout vl : varauksienTiedot) {
-                addComponent(vl);
+            // lisätään varaukset päänäkymään
+            for (VerticalLayout v : varauksienTiedot) {
+                addComponent(v);
             }
-
         }
     }
-
-    void addVaraus(Varaus varaus) {
-        repository.save(varaus);
-        update();
-    }
-
-
 }
